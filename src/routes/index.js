cf6a3cf613875f716a23db83266ddb3b1a2ffbdc
HamidTech <hammedowolabi2001@gmail.com>
2022-10-17 08:39:34 +0100

    initial setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,115 +4,14 @@ import { PrivateRoute, AuthRoute, PublicRoute } from "./config";
 
 
 //----------------Pages-------------------------//
-import Login from "../views/Auth/login";
-import Register from "../views/Auth/register";
-import Dashboard from "../views/Admin/dashboard";
-import NewRecord from "../views/Admin/new_record";
-import Admins from "../views/Admin/admins";
-import Uploaded from "../views/Admin/uploads";
-import ManageRecords from "../views/Admin/manage";
-import Draft from "../views/Admin/draft";
-import EditRecord from "../views/Admin/editRecord";
-import Profile from "../views/Admin/profile";
-import Trash from '../views/Admin/trash'
+import Home from "../views/Home";
+
 
 const AllRoutes = ()=>(
     <BrowserRouter>
         <Routes>
             
-            <Route path="/register" element={ <Register/>} />
-
-            <Route
-                path="/dashboard/new"
-                element={
-                    <PrivateRoute>
-                        <NewRecord/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/trash"
-                element={
-                    <PrivateRoute>
-                        <Trash/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/profile/:id"
-                element={
-                    <PrivateRoute>
-                        <Profile/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/edit/:id"
-                element={
-                    <PrivateRoute>
-                        <EditRecord/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/draft"
-                element={
-                    <PrivateRoute>
-                        <Draft/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/manage"
-                element={
-                    <PrivateRoute>
-                        <ManageRecords/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/uploads"
-                element={
-                    <PrivateRoute>
-                        <Uploaded/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard/admins"
-                element={
-                    <PrivateRoute>
-                        <Admins/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/dashboard"
-                element={
-                    <PrivateRoute>
-                        <Dashboard/>
-                    </PrivateRoute>
-                }
-            />
-
-            <Route
-                path="/"
-                exact
-                element={
-                    <PublicRoute>
-                        <Login/>
-                    </PublicRoute>
-                }
-            />
-
+            <Route path="/" element={ <Home/>} />
         </Routes>
     </BrowserRouter>
 )
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles/home.module.scss'
+
+const Home = () => {
+    return ( 
+        <div className={styles.wrapper}>
+           
+            <h1>KA E-Portal</h1>
+        </div>
+     );
+}
+ 
+export default Home;
