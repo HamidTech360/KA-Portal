8d588d063f6ddc4a65933032d1ed06751e420c5b
Yahckman <adeniranajibade2014@gmail.com>
2022-10-25 02:48:43 -0700

    Finishing of the side one of calender

--- src/views/Home/templates/calendar.jsx
+++ src/views/Home/templates/calendar.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import styles from './styles/calendar.module.scss'
-
+import React from "react";
+import styles from "./styles/calendar.module.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { AiOutlineDownload } from "react-icons/ai";
 const Calendar = () => {
-    return ( 
-        <div>
-            <h1>Calendar Template</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div className={styles.calender}>
+      <Container className={styles.calender_body}>
+        <Row>
+          <Col className={styles.calender_sideOne} xs={12} sm={6}>
+            <div className={styles.calender_line}></div>
+
+            <p className={styles.calender_para1}>
+              <span>View the </span> <br />
+              <span>2022/23 Calender</span>
+            </p>
+
+            <p className={styles.calender_para2}>
+              You can download the complete <br/> session calender below
+            </p>
+
+            <a href="#" className={styles.calender_button}>
+              Download Calender
+              <AiOutlineDownload size={29} style={{ paddingLeft: "10px" }} />
+            </a>
+          </Col>
+          <Col xs={12} sm={6}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
+            saepe rem nulla, laboriosam ipsa doloribus laborum maxime. Nihil,
+            praesentium? Tempore iste in quaerat? Voluptate adipisci id
+            doloremque? Necessitatibus, qui. Cum.
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
 export default Calendar;
