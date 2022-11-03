83d7d56205f32923cb524804348bb44e526c6d20
Yahckman <adeniranajibade2014@gmail.com>
2022-11-03 06:29:08 -0700

    Setting the responsive of the slipers

--- src/views/Home/templates/events.jsx
+++ src/views/Home/templates/events.jsx
@@ -4,7 +4,7 @@ import { Navigation, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import EventsCard from "./events/eventsCard";
 
-// Import Swiper styles
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -20,15 +20,33 @@ const Events = () => {
         <div className={styles.para1}>Upcoming Events</div>
       </div>
       <Swiper
+       breakpoints={{
+         320:{
+          width:320,
+          slidesPerView:1
+         },
+          640: {
+            width: 640,
+            slidesPerView: 1,
+          },
+         
+          768: {
+            width: 768,
+            slidesPerView: 2,
+          },
+        }}
         modules={[Navigation, Scrollbar]}
         spaceBetween={50}
-        slidesPerView={3}
+        slidesPerView={2}
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
         speed={400}
+        initialSlide={2}
+       
+        // className={styles.swiper_container}
       >
         <SwiperSlide>
             <EventsCard/>
