79df479c6df78e0afd35e8ef8c084c08aaf28f2f
HamidTech <hammedowolabi2001@gmail.com>
2022-11-12 10:07:39 +0100

    fixing about page

---
+++ src/utils/helpers/data/index.js
@@ -0,0 +1,14 @@
+export const ourValues = [
+    {
+        label:'Our Values',
+        item:['lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum']
+    },
+    {
+        label:'Our Mission',
+        item:['Mission texts here', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum']
+    },
+    {
+        label:'Our Vision',
+        item:['Vision texts here', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum']
+    }
+]
--- src/views/Home/templates/about.jsx
+++ src/views/Home/templates/about.jsx
@@ -1,66 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Row, Col } from 'react-bootstrap';
 import styles from './styles/about.module.scss'
 import {FaRegHeart, FaAngleDoubleRight }from "react-icons/fa"
+import { ourValues } from '../../../utils/helpers/data';
 import img from "../../../assets/Image.svg"
 
 const About = () => {
+    const [active, setActive] = useState(0)
+    const buttons = [
+        {label: 'Our values', icon:<FaRegHeart />},
+        {label: 'Our Mission', icon:''},
+        {label: 'Our Vision', icon:''}
+       
+    ]
     return ( 
         <div className = {styles.about}>
-            <div className= {styles.box}>
-                <div className={styles.sidebar1}>
-                    <h1>
-                    Welcome to
-                    Khayrul Adab School of
-                    Arabic and Islamic Studies
-                   </h1>
+            <Row className={`${styles.welcomeTab}`}>
+                <Col lg={6} md={6} sm={12} xs={12} className={styles.sidebar1}>
+                    <div className={styles.welcomeBold}> Welcome to Khayrul Adab School of Arabic and Islamic Studies</div>
                     <aside className={styles.icons}>
-                        <div className={styles.icon}>
-                            <FaRegHeart />
-                            <p>Our Values</p>
-                        </div>
-                        <div className={styles.icon}>
-                           
-                            <p>Our Mission</p>
-                        </div>
-                        <div className={styles.icon}>
-                        
-                            <p>Our Vision</p>
-                        </div>
+                        {buttons.map((item, index)=>
+                            <button
+                                    style={{backgroundColor:active==index && ' #1A8F4A', color:active==index && 'white'}} 
+                                    className={styles.icon}
+                                    onClick={()=>setActive(index)}
+                            >
+                                {item.icon}
+                                <p className='ml-3'>{item.label}</p>
+                            </button>
+                        )}
+                       
                     </aside>
+
+                   <div className="tabList">
+                        {ourValues[active].item.map((item, i)=>
+                        <div className={styles.flex}>
+                            <div className={styles.flexicon} >
+                                <FaAngleDoubleRight/>
+                            </div>   
+                            <p>{item}</p>
+                        </div>
+                        )}  
+                   </div>
                    
-                   <div className={styles.flex}>
-                   <div className={styles.flexicon} >
-                           <FaAngleDoubleRight/>
-                       </div>
-                       <p>Lorem Ipsum</p>
-                   </div>
-                   <div className={styles.flex}>
-                   <div className={styles.flexicon} >
-                           <FaAngleDoubleRight/>
-                       </div>
-                       <p>Purus tempus purus</p>
-                   </div>
-                   <div className={styles.flex}>
-                       <div className={styles.flexicon} >
-                           <FaAngleDoubleRight/>
-                       </div>
-                       <p>Ultricies sit purus, scelerisque</p>
-                   </div>
-                   <div  className={styles.flex}>
-                   <div className={styles.flexicon} >
-                           <FaAngleDoubleRight/>
-                       </div>
-                       <p>Lorem Ipsum feugiat</p>
-                   </div>
-                   <div  className={styles.flex}>
-                   <div className={styles.flexicon} >
-                           <FaAngleDoubleRight/>
-                       </div>
-                       <p>Lorem Ipsum</p>
-                   </div>
+                   
                       
-                </div>
-                <aside className={styles.sidebar2}>
+                </Col>
+
+                <Col lg={6} md={6} sm={12} xs={12}  className={styles.sidebar2}>
                     <img className={styles.img} src= {img} alt="Al-Qur'an" width={350} height={250} />
                     <div className={styles.article}>
                         <h1>About Our Institution</h1>
@@ -77,58 +64,63 @@ const About = () => {
                           Ultricies sit purus, scelerisque malesuada orci sed quis vel, commodo. 
                                        </p>
                     </div>
-                </aside>
-            </div>
+                </Col>
+            </Row>
 
-                <div className={styles.footerwrap}>
-                    <div>
-                    <iframe src={img} frameborder="0"></iframe>
-                    </div>
-                    
-                    
-                                <div className={styles.foot}>
+
+
+            <div className={styles.footerwrap}>
+                    {/* <div> 
+                        <iframe src={img} frameborder="0"></iframe> 
+                    </div> */}
+
+                    <div className={styles.foot}>
                          
-                                <div className={styles.que}>
-                        <h1 >Is <span>Khayrul Adab School</span> the right
-                    choice for you?</h1>
+                        <div className={styles.que}>
+                            <h1 >Is <span>Khayrul Adab School</span> the right choice for you?</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Tortor nisl eget orci magna malesuada ipsum turpis eu.
+                                Tortor cursus.
+                            </p>
+                        </div>
+
+                        <div className={styles.footer}>
+                            <div className={styles.footitem}>
+                                <h4>Program Title</h4>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                     Tortor nisl eget orci magna malesuada ipsum turpis eu.
-                      Tortor cursus.</p>
-                                </div>
-                    <div className={styles.footer}>
-                        <div className={styles.footitem}>
-                            <h4>Program Title</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
-                                est sagittis duis arcu, pretium.
-                                Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
+                                    Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
+                                    est sagittis duis arcu, pretium.
+                                    Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
                                 </p>
-                        </div>
-                        <div className={styles.footitem}>
-                            <h4>Program Title</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
-                                est sagittis duis arcu, pretium.
-                                Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
+                            </div>
+                            <div className={styles.footitem}>
+                                <h4>Program Title</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
+                                    est sagittis duis arcu, pretium.
+                                    Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
                                 </p>
-                        </div>
-                        <div className={styles.footitem}>
-                            <h4>Program Title</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
-                                est sagittis duis arcu, pretium.
-                                Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
+                            </div>
+                            <div className={styles.footitem}>
+                                <h4>Program Title</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
+                                    est sagittis duis arcu, pretium.
+                                    Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
                                 </p>
-                        </div>
-                        <div className={styles.footitem}>
-                            <h4>Program Title</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
-                                est sagittis duis arcu, pretium.
-                                Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
+                            </div>
+                            <div className={styles.footitem}>
+                                <h4>Program Title</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Feugiat accumsan urna libero molestie. Laoreet sed tempus augue ipsum,
+                                    est sagittis duis arcu, pretium.
+                                    Consectetur nec, eget amet, vitae facilisi dui, tristique arcu.
                                 </p>
+                            </div>
                         </div>
-                    </div>
+
+
                 </div>
             </div>
         </div>
