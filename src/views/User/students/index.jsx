603f39adad45179f8ad49525fa5cafa17006a7ee
HamidTech <hammedowolabi2001@gmail.com>
2022-11-18 16:38:33 +0100

    adding new files

---
+++ src/views/User/students/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/student.module.scss'
+
+const Students = () => {
+    return ( 
+        <div className={styles.studentScreen}>
+            students table goes here
+        </div>
+     );
+}
+ 
+export default Students;
