bbabc008cc93e179500609621a6f50971b7111c0
Yahckman <adeniranajibade2014@gmail.com>
2022-11-23 02:01:55 -0800

    adding the title

--- src/views/Resources/templates/lecture/programme.js
+++ src/views/Resources/templates/lecture/programme.js
@@ -12,7 +12,7 @@
     },
     {
         src:'https://www.facebook.com/Amubieyaonline/video/436220210937772/?flite=scwspnss&mibextid=EfcE3EL5YAUxHY5n',
-        para1:'Leters to a Young Muslim',
+        para1:'Lost Islamic History',
         para2:'Author: Omar Sahir Gbohar',
         para3:'Genres: Biography, Autobiography',
         para4:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
