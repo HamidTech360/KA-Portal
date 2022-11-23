a23654ebbfeb9e1bda70e30b120b0f8ec378e851
Yahckman <adeniranajibade2014@gmail.com>
2022-11-23 01:59:39 -0800

    the programms

---
+++ src/views/Resources/templates/lecture/programme.js
@@ -0,0 +1,57 @@
+  export const  programmes=[
+    {
+        src:'https://www.facebook.com/Amubieyaonline/video/436220210937772/?flite=scwspnss&mibextid=EfcE3EL5YAUxHY5n',
+        para1:'Leters to a Young Muslim',
+        para2:'Author: Omar Sahir Gbohar',
+        para3:'Genres: Biography, Autobiography',
+        para4:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+        pharetra arcu ipsum elementum erat egestas. Nisl condimentum eget id
+        ac eu dolor aliquet et molestie. Sed pulvinar erat mauris pretium
+        egestas orci.`,
+        link:'#'
+    },
+    {
+        src:'https://www.facebook.com/Amubieyaonline/video/436220210937772/?flite=scwspnss&mibextid=EfcE3EL5YAUxHY5n',
+        para1:'Leters to a Young Muslim',
+        para2:'Author: Omar Sahir Gbohar',
+        para3:'Genres: Biography, Autobiography',
+        para4:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+        pharetra arcu ipsum elementum erat egestas. Nisl condimentum eget id
+        ac eu dolor aliquet et molestie. Sed pulvinar erat mauris pretium
+        egestas orci.`,
+        link:'#'
+    },
+    {
+        src:'https://www.facebook.com/Amubieyaonline/video/436220210937772/?flite=scwspnss&mibextid=EfcE3EL5YAUxHY5n',
+        para1:'Leters to a Young Muslim',
+        para2:'Author: Omar Sahir Gbohar',
+        para3:'Genres: Biography, Autobiography',
+        para4:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+        pharetra arcu ipsum elementum erat egestas. Nisl condimentum eget id
+        ac eu dolor aliquet et molestie. Sed pulvinar erat mauris pretium
+        egestas orci.`,
+        link:'#'
+    },
+    {
+        src:'https://www.facebook.com/Amubieyaonline/video/436220210937772/?flite=scwspnss&mibextid=EfcE3EL5YAUxHY5n',
+        para1:'Leters to a Young Muslim',
+        para2:'Author: Omar Sahir Gbohar',
+        para3:'Genres: Biography, Autobiography',
+        para4:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+        pharetra arcu ipsum elementum erat egestas. Nisl condimentum eget id
+        ac eu dolor aliquet et molestie. Sed pulvinar erat mauris pretium
+        egestas orci.`,
+        link:'#'
+    },
+    {
+        src:'https://www.facebook.com/Amubieyaonline/video/436220210937772/?flite=scwspnss&mibextid=EfcE3EL5YAUxHY5n',
+        para1:'Leters to a Young Muslim',
+        para2:'Author: Omar Sahir Gbohar',
+        para3:'Genres: Biography, Autobiography',
+        para4:`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+        pharetra arcu ipsum elementum erat egestas. Nisl condimentum eget id
+        ac eu dolor aliquet et molestie. Sed pulvinar erat mauris pretium
+        egestas orci.`,
+        link:'#'
+    },
+]
