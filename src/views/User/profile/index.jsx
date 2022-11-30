c0faaa87c44d1604fa22dc13b564743f295ffe9a
Yahckman <adeniranajibade2014@gmail.com>
2022-11-30 03:00:59 -0800

    the student profile

--- src/views/User/profile/index.jsx
+++ src/views/User/profile/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./styles/profile.module.scss";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import {TbMinusVertical} from 'react-icons/tb'
+
 import pics from "./../../../assets/pass.png";
 const StudentProfile = () => {
   const studentsProfile=[
@@ -64,33 +66,45 @@ const StudentProfile = () => {
   ]
   return (
     <div className={styles.profile}>
-      {/* <h1>Student profile</h1> */}
-      <div className={styles.para1_container}>
-        <p className={styles.para1}>Student Details</p>
-        <span className={styles.menu_btn}>
-          {" "}
-          <HiOutlineDotsHorizontal />{" "}
-        </span>
-      </div>
-
-      <div className="row">
-        <div className={`col-12 col-md-4`}>
-          <img src={pics} alt="Passport" className={styles.passport} />
-        </div>
-        <div className={`${styles.student_details} col-12 col-md-8`} >
-          <p className={styles.para2}> Abdullah Fatah </p>
-          <p className={styles.para4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et urna,
-            laoreet ullamcorper ac at. Et amet, malesuada scelerisque nisi velit
-            id.{" "}
+      <div className={styles.profile_top}>
+          <p className={styles.para6}>
+            <span  className={styles.para6_main}>All Students</span>
+            <span className={styles.para6_line}> | </span>
+            <span className={styles.sub}>248 students</span>
           </p>
-      {studentsProfile.map((student,i)=>(
-
-          <div className={styles.para5} key={i} >
-            <p  className={styles.para5_main}>{student.main}</p>
-            <p className={styles.para5_sub}>{student.sub}</p>
+          <p className={styles.para7}>
+            <span className={styles.para7_main}>Students  </span>
+            <span className={styles.para7_icon}> &gt; </span>
+            <span className={styles.para7_sub}>Abdullah Fatah  </span>
+          </p>
+      </div>
+      <div className={styles.profile_body}>
+        {/* <h1>Student profile</h1> */}
+        <div className={styles.para1_container}>
+          <p className={styles.para1}>Student Details</p>
+          <span className={styles.menu_btn}>
+            {" "}
+            <HiOutlineDotsHorizontal size={40} />{" "}
+          </span>
+        </div>
+        <div className="row">
+          <div className={`col-12 col-md-4`}>
+            <img src={pics} alt="Passport" className={styles.passport} />
           </div>
-      ))}
+          <div className={`${styles.student_details} col-12 col-md-8`} >
+            <p className={styles.para2}> Abdullah Fatah </p>
+            <p className={styles.para4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et urna,
+              laoreet ullamcorper ac at. Et amet, malesuada scelerisque nisi velit
+              id.{" "}
+            </p>
+        {studentsProfile.map((student,i)=>(
+            <div className={styles.para5} key={i} >
+              <p  className={styles.para5_main}>{student.main}</p>
+              <p className={styles.para5_sub}>{student.sub}</p>
+            </div>
+        ))}
+          </div>
         </div>
       </div>
     </div>
