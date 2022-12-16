446f7a12062e27645d60101e759db65e526cd6f8
Yahckman <adeniranajibade2014@gmail.com>
2022-12-16 02:05:50 -0800

    testing

---
+++ src/views/User/studentResult/studentResult.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResultTable from '../../../components/Table/resultTable';
+
+function StudentResult(props) {
+    return (
+        <div>
+            
+
+            < ResultTable/>
+        </div>
+    );
+}
+
+export default StudentResult;
