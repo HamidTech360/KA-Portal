63401dbb131d4e9ba1ee3e3c6da53dd7f4d69996
Adeniran Yaqub <adeniranajibade2014@gmail.com>
2022-12-26 08:35:11 -0800

    Setting up the result page

--- src/views/User/studentResult/index.jsx
+++ src/views/User/studentResult/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ResultTable from '../../../components/Table/resultTable';
-
+import styles from './style/studentResult.module.scss'
 function StudentResult(props) {
     return (
-        <div>
-            <h1> Studen Result</h1>
+        <div className={styles.result}>
+            <div>
+                <div className={styles.para1}>View Examination Results</div>
+                <div className={styles.para2}>
+                     <span>Home</span>
+                    <span className={styles.para2_logo}>&gt;</span>
+                    <span className={styles.para2_sub}>Student Result</span>
+                 </div>
+            </div>
 
-            < ResultTable/>
+            {/* < ResultTable/> */}
         </div>
     );
 }
