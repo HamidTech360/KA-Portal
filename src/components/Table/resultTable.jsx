ad4eb3c67e505c275d0c05e05c379a718c25252a
Adeniran Yaqub <adeniranajibade2014@gmail.com>
2022-12-27 05:59:31 -0800

    table cont

--- src/components/Table/resultTable.jsx
+++ src/components/Table/resultTable.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import styles from './resultTable.module.scss'
 function ResultTable({tableHeader,results}) {
  
-    if (results.score<50){
-        results.grade='fail'
-    }else if (results.score >=50 || results.score<69 ){
-        results.grade= 'Average'
-    }else if (results.score >=70 || results.score<80){
-        results.grade='Very Good'
+    // if (results.score<50){
+    //     results.grade='fail'
+    // }else if (results.score >=50 || results.score<69 ){
+    //     results.grade= 'Average'
+    // }else if (results.score >=70 || results.score<80){
+    //     results.grade='Very Good'
 
-    }else {
-        results.grade='Excellent'
-    }
-    console.log(results);
+    // }else {
+    //     results.grade='Excellent'
+    // }
+    // console.log(results);
+
+    // let classOption = results.score<50? `${styles.fail}`: `${styles.pass}`
     return (
         <div>
             <table >
@@ -28,7 +30,8 @@ function ResultTable({tableHeader,results}) {
                     <tr className={styles.trows}>
                         <td>{result.courseCode}</td>
                         <td>{result.courseTitle}</td>
-                        <td className={result.score<50? `${styles.fail}`: `${styles.pass}`}>{result.score<50 ? result.pass='fail': result.pass='pass'}</td>
+                        {/* <td className={classOption}>{result.score<50 ? result.pass='fail': result.pass='pass'}</td> */}
+                        <td > <span className={result.score<50? `${styles.fail}`: `${styles.pass}`}>{result.score<50 ? result.pass='fail': result.pass='pass'}</span> </td>
                         <td>{result.score}%</td>
                         <td>{result.grade}</td>
                         <td>{result.dots}</td>
