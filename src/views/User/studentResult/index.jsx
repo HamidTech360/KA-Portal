a7d42bf7addc424f9e9b6c8e9eed3f69ed863b4d
Adeniran Yaqub <adeniranajibade2014@gmail.com>
2022-12-27 04:28:50 -0800

    table

--- src/views/User/studentResult/index.jsx
+++ src/views/User/studentResult/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ResultTable from '../../../components/Table/resultTable';
 import styles from './style/studentResult.module.scss'
 import passport from './../../../assets/pass.png'
+import Footer from './../../../components/Footer/footer';
 function StudentResult(props) {
     const tableHeader=[
         {label:'Course code'},
@@ -22,7 +23,7 @@ function StudentResult(props) {
             courseCode:'IRS101',
             courseTitle:'Islamic Religion Studies',
             pass:'pass',
-            score:75 
+            score:32 
         },
         {
             courseCode:'IRS101',
@@ -34,7 +35,13 @@ function StudentResult(props) {
             courseCode:'IRS101',
             courseTitle:'Islamic Religion Studies',
             pass:'pass',
-            score:75 
+            score:68 
+        },
+        {
+            courseCode:'IRS101',
+            courseTitle:'Islamic Religion Studies',
+            pass:'pass',
+            score:32 
         },
         {
             courseCode:'IRS101',
@@ -46,8 +53,15 @@ function StudentResult(props) {
             courseCode:'IRS101',
             courseTitle:'Islamic Religion Studies',
             pass:'pass',
-            score:10 
+            score:32 
         },
+        {
+            courseCode:'IRS101',
+            courseTitle:'Islamic Religion Studies',
+            pass:'pass',
+            score:75 
+        },
+        
     ]
     const details = [
         {
@@ -69,46 +83,47 @@ function StudentResult(props) {
        
     ]
 
+    const
+
     return (
-        <div className={styles.result}>
-            <div>
-                <div className={styles.para1}>View Examination Results</div>
-                <div className={styles.para2}>
-                     <span>Home</span>
-                    <span className={styles.para2_logo}>&gt;</span>
-                    <span className={styles.para2_sub}>Student Result</span>
-                 </div>    
-            </div>
-
-            <div className={`row ${styles.studentDetails} `}>
-                <div className={`col-12 col-md-3 col-lg-3 ${styles.pass_container}`}>
-                    <img className={styles.pass} src={passport} alt='student passport' />
+        <div>
+            <div className={styles.result}>
+                <div>
+                    <div className={styles.para1}>View Examination Results</div>
+                    <div className={styles.para2}>
+                         <span>Home</span>
+                        <span className={styles.para2_logo}>&gt;</span>
+                        <span className={styles.para2_sub}>Student Result</span>
+                     </div>
                 </div>
-                <div className="col-12 col-md-8 col-lg-8">
-                    <p className={styles.para4}>Abdullah Fattah</p>
-                    {details.map((detail,i)=>(
-                    <div key={i} className={styles.details}>
-                        <span className={styles.details_main}>{detail.main}</span>
-                        <span className={styles.details_sub}>{detail.sub}</span>
+                <div className={`row ${styles.studentDetails} `}>
+                    <div className={`col-12 col-md-3 col-lg-3 ${styles.pass_container}`}>
+                        <img className={styles.pass} src={passport} alt='student passport' />
                     </div>
-                    ))}
-
-                </div>
-
-            </div>
-                    
-                    <div className={styles.studentTable}>
-                        <div className={styles.table_title}>
-                        <p className={styles.table_title_main}>Results for WINTER SEMESTER 2018/2019 SESSION</p>
-                            <p className={styles.table_title_sub}> List of offered courses</p>
+                    <div className="col-12 col-md-8 col-lg-8">
+                        <p className={styles.para4}>Abdullah Fattah</p>
+                        {details.map((detail,i)=>(
+                        <div key={i} className={styles.details}>
+                            <span className={styles.details_main}>{detail.main}</span>
+                            <span className={styles.details_sub}>{detail.sub}</span>
                         </div>
-                        
-                    < ResultTable
-                        tableHeader={tableHeader}
-                        results={results}
-                    />
+                        ))}
                     </div>
-
+                </div>
+            
+                        <div className={styles.studentTable}>
+                            <div className={styles.table_title}>
+                            <p className={styles.table_title_main}>Results for WINTER SEMESTER 2018/2019 SESSION</p>
+                                <p className={styles.table_title_sub}> List of offered courses</p>
+                            </div>
+            
+                        < ResultTable
+                            tableHeader={tableHeader}
+                            results={results}
+                        />
+                        </div>
+            </div>
+                
         </div>
     );
 }
