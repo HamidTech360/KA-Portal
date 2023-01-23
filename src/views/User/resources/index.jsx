32b6ef452d73ab3d4e979c9b0def9a896f879b02
Adeniran Yaqub <adeniranajibade2014@gmail.com>
2023-01-23 03:37:53 -0800

    setting the upper body

--- src/views/User/resources/index.jsx
+++ src/views/User/resources/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import styles from "./styles/resources.module.scss";
+import { FiUpload } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 function Resources(props) {
-    return (
-        <div>
-            <h1>Resfghjkl;</h1>
-        </div>
-    );
+  return (
+    <div className={styles.resources}>
+      <p className={styles.para1}>
+        <span className={styles.para1_main}> Resources</span>
+        <span >
+          {" "}
+          <Link to='#' className={styles.para1_sub}>
+          
+          <FiUpload size={20} className={styles.upload} /> <span className="hideOnMobile">Upload Resources</span> 
+          </Link>
+        </span>
+      </p>
+    </div>
+  );
 }
 
 export default Resources;
