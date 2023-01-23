791fc516754729304a89327649e829dd9d3faa7e
Adeniran Yaqub <adeniranajibade2014@gmail.com>
2023-01-23 04:54:49 -0800

    finishing the card

--- src/views/User/resources/index.jsx
+++ src/views/User/resources/index.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import styles from "./styles/resources.module.scss";
 import { FiUpload } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import LectureCard from "./lectureCard";
+import img from "./../../../assets/news2.jpg";
+
 function Resources(props) {
+  const cardInfo = [
+    {
+      img: img,
+      name: "Dr Zakir NAikh",
+      title: "The Benefits of Patience",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra arcu ipsum elementum erat egestas...",
+    },
+    {
+      img: img,
+      name: "Dr Zakir NAikh",
+      title: "The Benefits of Patience",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra arcu ipsum elementum erat egestas...",
+    },
+    {
+      img: img,
+      name: "Dr Zakir NAikh",
+      title: "The Benefits of Patience",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra arcu ipsum elementum erat egestas...",
+    },
+  ];
   return (
     <div className={styles.resources}>
       <div className={styles.para1}>
@@ -16,20 +39,31 @@ function Resources(props) {
         </span>
       </div>
 
-        <p className={styles.breadcrumb}>
-            <span  className={styles.breadcrumb_para1}>Home</span>
-            <span className={styles.breadcrumb_para2}>&gt;</span>
-            <span className={styles.breadcrumb_para3}>Resouces</span>   
-        </p>
+      <p className={styles.breadcrumb}>
+        <span className={styles.breadcrumb_para1}>Home</span>
+        <span className={styles.breadcrumb_para2}>&gt;</span>
+        <span className={styles.breadcrumb_para3}>Resouces</span>
+      </p>
 
       <div className={styles.lectures}>
+        <p className={styles.para2}>
+          <span className={styles.para2_main}>Lectures</span>
+          <span className={styles.para2_sub}></span>
+        </p>
 
-      <p className={styles.para2}>
-        <span className={styles.para2_main}>Lectures</span>
-        <span className={styles.para2_sub}></span>
-      </p>
+        <div className="row" style={{ paddingTop: "30px" }}>
+          {cardInfo.map((info, i) => (
+            <div className="col-12 col-lg-4 col-md-6">
+              <LectureCard
+                image={info.img}
+                name={info.name}
+                title={info.title}
+                body={info.body}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-
     </div>
   );
 }
