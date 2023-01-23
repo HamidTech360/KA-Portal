58d78aa724b29cc1384eac170685dc588c32f564
Adeniran Yaqub <adeniranajibade2014@gmail.com>
2023-01-23 04:00:03 -0800

    upper

--- src/views/User/resources/index.jsx
+++ src/views/User/resources/index.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import styles from "./styles/resources.module.scss";
 import { FiUpload } from "react-icons/fi";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 function Resources(props) {
   return (
     <div className={styles.resources}>
-      <p className={styles.para1}>
+      <div className={styles.para1}>
         <span className={styles.para1_main}> Resources</span>
-        <span >
+        <span>
           {" "}
-          <Link to='#' className={styles.para1_sub}>
-          
-          <FiUpload size={20} className={styles.upload} /> <span className="hideOnMobile">Upload Resources</span> 
+          <Link to="#" className={styles.para1_sub}>
+            <FiUpload size={20} className={styles.upload} />{" "}
+            <span className="hideOnMobile">Upload Resources</span>
           </Link>
         </span>
+      </div>
+
+        <p className={styles.breadcrumb}>
+            <span  className={styles.breadcrumb_para1}>Home</span>
+            <span className={styles.breadcrumb_para2}>&gt;</span>
+            <span className={styles.breadcrumb_para3}>Resouces</span>   
+        </p>
+
+      <div className={styles.lectures}>
+
+      <p className={styles.para2}>
+        <span className={styles.para2_main}>Lectures</span>
+        <span className={styles.para2_sub}></span>
       </p>
+      </div>
+
     </div>
   );
 }
